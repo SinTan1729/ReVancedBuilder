c6b18f3225361ca35ea244b9a0da946020086ade
SinTan1729 <35728385+SinTan1729@users.noreply.github.com>
2022-10-09 17:53:35 -0500

    Disable building root apks

--- build_revanced.sh
+++ build_revanced.sh
@@ -89,11 +89,11 @@ echo "************************************"
 mkdir -p build
 
 if [ -f "com.google.android.youtube.apk" ]; then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
-        -e microg-support ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.youtube.apk -o build/revanced-root.apk
+#    echo "Building Root APK"
+#    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
+#        -e microg-support ${patches[@]} \
+#        $EXPERIMENTAL \
+#        -a com.google.android.youtube.apk -o build/revanced-root.apk
     echo "Building Non-root APK"
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
         ${patches[@]} \
@@ -107,11 +107,11 @@ echo "************************************"
 echo "Building YouTube Music APK"
 echo "************************************"
 if [ -f "com.google.android.apps.youtube.music.apk" ]; then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-        -e microg-support ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
+#    echo "Building Root APK"
+#    java -jar revanced-cli.jar -b revanced-patches.jar --mount \
+#        -e microg-support ${patches[@]} \
+#        $EXPERIMENTAL \
+#        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
     echo "Building Non-root APK"
     java -jar revanced-cli.jar -b revanced-patches.jar \
         ${patches[@]} \
